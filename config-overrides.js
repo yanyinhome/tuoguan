4accd755075aa1f020628b52b90ec4075abf41a1
yanm <laoyan1025@163.com>
2020-06-07 22:51:50 +0800

    postcss-px2rem antd解决

--- config-overrides.js
+++ config-overrides.js
@@ -16,9 +16,5 @@ module.exports = override(
     /* 解除使用public限制 */
     removeModuleScopePlugin(),
     /*  自适应转换 */
-<<<<<<< HEAD
     addPostcssPlugins([px2rem({remUnit: 75, exclude: /node_modules/i})]),
-=======
-    addPostcssPlugins([px2rem({remUnit: 75, exclude: './node_modules/i'})]),
->>>>>>> 2fc009ba0f9cfc52d22b61aaf91e67c58bf90d74
 );
